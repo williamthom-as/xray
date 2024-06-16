2b4ca417cc4ed686b9dfcd446ec2ab7699852057
william <me@williamthom.as>
2024-06-16 21:04:21 +1000

    feat: exportable dashboards saved to json

--- src/dialog/object_viewer_dialog.js
+++ src/dialog/object_viewer_dialog.js
@@ -18,8 +18,25 @@ export class ObjectViewerDialog {
 
     if (model.object) {
       this.object = model.object;
-      this.json = JSON.stringify(model.object);
+      this.json = JSON.stringify(model.object, null, 2);
     }
+  }
+
+  downloadTemplate() {
+    const title = this.title.toLowerCase().replace(/ /g, '_')
+    const blob = new Blob([this.json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    
+    const a = Object.assign(
+      document.createElement('a'), 
+      { style: { display: 'none' }, href: url, download: `${title}.json` }
+    );
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
   }
 
 }
