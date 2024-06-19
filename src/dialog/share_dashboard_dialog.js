2d7078205e31c3683eeabe6076f93ec22f3fb401
william <me@williamthom.as>
2024-06-19 23:38:38 +1000

    feat: add jsoncrush algo for sharing dashboards

--- src/dialog/share_dashboard_dialog.js
+++ src/dialog/share_dashboard_dialog.js
@@ -1,5 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {DialogController} from 'aurelia-dialog-lite';
+import JSONCrush from 'jsoncrush';
 
 @inject(DialogController, 'AppService')
 export class ShareDashboardDialog {
@@ -18,9 +19,32 @@ export class ShareDashboardDialog {
     if (this.dashboard.gistId) {
       this.urlString = `${this.hostAddr}/dashboard/viewer/remote?gistId=${this.dashboard.gistId}`;
     } else {
-      const encoded = btoa(JSON.stringify(this.dashboard.content));
+      const jsonStr = JSON.stringify(this.dashboard.content);
+      const encoded = btoa(jsonStr);
       this.urlString = `${this.hostAddr}/dashboard/viewer/remote?encoded=${encoded}`;
     }
+  }
+
+  attached() {
+    const jsonStr = JSON.stringify(this.dashboard.content);
+
+    this.createJSONCrushURL(jsonStr)
+      .then((jsonCrushStr) => {
+        this.jsonCrushStr = jsonCrushStr;
+      })
+      .catch((e) => {
+        console.err(e)
+      })
+  }
+
+  createJSONCrushURL(jsonStr) {
+    return new Promise((resolve) => {
+      const jsoncrush = encodeURIComponent(JSONCrush.crush(jsonStr));
+      
+      resolve(
+        `${this.hostAddr}/dashboard/viewer/remote?jsoncrush=${jsoncrush}`
+      );
+    });
   }
 
   copy() {
@@ -32,4 +56,27 @@ export class ShareDashboardDialog {
         this.app.showError("Error", "Failed to copy to clipboard. Please manually select text.");
       });
   }
+
+  downloadTemplate() {
+    let title = "dashbard_template"
+    if (this.dashboard.content) {
+      title = this.dashboard.content.title.toLowerCase().replace(/ /g, '_')
+    }
+
+    const dashboardJSON = JSON.stringify(this.dashboard.content)
+    const blob = new Blob([dashboardJSON], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    
+    const a = Object.assign(
+      document.createElement('a'), 
+      { style: { display: 'none' }, href: url, download: `${title}.json` }
+    );
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  }
+
 }
